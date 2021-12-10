a13abf10e4a9cf12e45f335532b678f634dfbd95
Stijn-Jacobs <stijn.jacobs33@gmail.com>
2021-12-11 00:17:33 +0100

    Added day 7 of 2021.

---
+++ 2021/7/code.py
@@ -0,0 +1,28 @@
+# Advent of code Year 2021 Day 7 solution
+# Author = Stijn-Jacobs
+# Date = December 2021
+
+with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
+    input = input_file.readline()
+
+
+def find_least_movements_position(inp, exponential):
+    arr = list(map(int, inp.split(",")))
+    fuels_spent_arr = []
+
+    for target in range(min(arr), max(arr)):
+        fuel_spent = 0
+        for num in arr:
+            if exponential:
+                fuel_spent += abs(num - target) * (abs(num - target) + 1) // 2
+            else:
+                fuel_spent += abs(num - target)
+
+        fuels_spent_arr.append(fuel_spent)
+
+    return min(fuels_spent_arr)
+
+
+print("Part One : " + str(find_least_movements_position(input, False)))
+
+print("Part Two : " + str(find_least_movements_position(input, True)))
