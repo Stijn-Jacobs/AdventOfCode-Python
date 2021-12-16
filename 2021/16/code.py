c07ae9317766c1e6e1c3f8a102bd06783fd139a6
Stijn-Jacobs <stijn.jacobs33@gmail.com>
2021-12-16 14:44:51 +0100

    Added part 2 day 16 of 2021.

--- 2021/16/code.py
+++ 2021/16/code.py
@@ -1,6 +1,8 @@
 # Advent of code Year 2021 Day 16 solution
 # Author = Stijn-Jacobs
 # Date = December 2021
+
+import numpy as np
 from math import floor
 
 with open((__file__.rstrip("code.py") + "input.txt"), 'r') as input_file:
@@ -11,12 +13,11 @@ class Packet:
     def __init__(self, decode_from):
         self.version = None
         self.type_id = None
-        self.value = None
+        self._value = None
         self.sub_packets = []
         self.length = self.decode_from(decode_from)
 
     def decode_from(self, inp):
-        print("from: " + str(inp))
         self.version = int(inp[0:3], 2)
         self.type_id = int(inp[3:6], 2)
         inp = inp[6:]
@@ -28,8 +29,8 @@ class Packet:
                 literal_value_binary += val[1:]
                 if int(val[0]) == 0:
                     # Last group of packet
-                    self.value = int(literal_value_binary, 2)
-                    return 6 + (group * 5) + 5
+                    self._value = int(literal_value_binary, 2)
+                    return (group * 5) + 5
         else:
             # Ignore operators for now
             length_id = int(inp[0])
@@ -43,9 +44,9 @@ class Packet:
                     packet = Packet(inp[length_sum:])
                     self.sub_packets.append(packet)
                     length_sum += packet.packet_length()
-                return 6 + length_sum + 1 + 15
+                return length_sum + 1 + 15
             else:
-                # Next 11 bits indicate the amount of subpackets
+                # Next 11 bits indicate the amount of sub-packets
                 amount = int(inp[0:11], 2)
                 inp = inp[11:]
                 index = 0
@@ -53,14 +54,30 @@ class Packet:
                     packet = Packet(inp[index:])
                     self.sub_packets.append(packet)
                     index += packet.packet_length()
-                return 6 + index + 1 + 11
+                return index + 1 + 11
 
     def packet_length(self):
-        return self.length
+        return self.length + 6
 
-    def __repr__(self):
-        return "V: " + str(self.version) + ", Type: " + str(self.type_id) + " Value: " + str(self.value) + ", " + str(
-            [str(pack) for pack in self.sub_packets])
+    def get_value(self):
+        if self._value:
+            return self._value
+        sub_values = [sub.get_value() for sub in self.sub_packets]
+        if self.type_id == 0:
+            return sum(sub_values)
+        elif self.type_id == 1:
+            return sub_values[0] if len(sub_values) == 1 else np.prod(sub_values)
+        elif self.type_id == 2:
+            return min(sub_values)
+        elif self.type_id == 3:
+            return max(sub_values)
+        elif self.type_id == 5:
+            return 1 if sub_values[0] > sub_values[1] else 0
+        elif self.type_id == 6:
+            return 1 if sub_values[0] < sub_values[1] else 0
+        elif self.type_id == 7:
+            return 1 if sub_values[0] == sub_values[1] else 0
+        return None
 
     def version_sum(self):
         version_sum = 0
@@ -70,15 +87,10 @@ class Packet:
 
 
 def binary_representation(inp):
-    arr = ""
-    for char in inp:
-        arr += bin(int(char, 16))[2:].zfill(4)
-    return arr
+    return "".join([bin(int(char, 16))[2:].zfill(4) for char in inp])
 
 
 root_packet = Packet(binary_representation(input))
-for packet in root_packet.sub_packets:
-    print(packet)
 print("Part One : " + str(root_packet.version_sum()))
 
-print("Part Two : " + str(None))
+print("Part Two : " + str(root_packet.get_value()))
