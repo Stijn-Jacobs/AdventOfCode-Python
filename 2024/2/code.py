a8b2dbe22dbe67e307937dd4690568cbe9b20a99
Stijn-Jacobs <stijn.jacobs33@gmail.com>
2024-12-06 22:08:13 +0100

    2024 day 2 cleanup

--- 2024/2/code.py
+++ 2024/2/code.py
@@ -12,18 +12,13 @@ def parse_input(input):
 def get_diffs(line):
     return [line[j] - line[j + 1] for j in range(len(line) - 1)]
 
+def is_valid_diff(diff_arr):
+    return all([abs(diff) in range(1, 4) for diff in diff_arr]) and \
+           (all(diff > 0 for diff in diff_arr) or all(diff < 0 for diff in diff_arr))
+
 def solve1(input):
-    sum = 0
-
     diffs = [get_diffs(i) for i in parse_input(input)]
-
-    for diff_arr in diffs:
-        if all([abs(diff) in range(1, 4) for diff in diff_arr]):
-            if all(diff > 0 for diff in diff_arr) or all(diff < 0 for diff in diff_arr):
-                sum += 1
-
-
-    return sum
+    return sum([is_valid_diff(diff_arr) for diff_arr in diffs])
 
 def solve2(input):
     sum = 0
@@ -31,16 +26,13 @@ def solve2(input):
     for line in parse_input(input):
         for i in range(-1, len(line)):
             copy = line
-            if i >= 0: # remove element and check if valid that way
+            if i >= 0:  # remove element and check if valid that way
                 copy = line[:i] + line[i + 1:]
-
             diff_arr = get_diffs(copy)
-            if all([abs(diff) in range(1, 4) for diff in diff_arr]):
-                if all(diff > 0 for diff in diff_arr) or all(diff < 0 for diff in diff_arr):
-                    sum += 1
-                    break
+            if is_valid_diff(diff_arr):
+                sum += 1
+                break
     return sum
 
 print(solve1(input))
-
 print(solve2(input))
